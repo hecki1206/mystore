7bdc74540eb1f447d2b6c62c7793233496cc33eb
hecki <92073357+hecki1206@users.noreply.github.com>
2023-12-04 14:55:06 +0900

    Add files via upload

---
+++ test01.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+# 데이터 생성
+x = [1, 2, 3, 4, 5]
+y = [10, 15, 7, 12, 9]
+
+# 선 그래프
+plt.plot(x, y)
+plt.xlabel('X 축')
+plt.ylabel('Y 축')
+plt.title('간단한 선 그래프')
+plt.show()
---
+++ test02.py
@@ -0,0 +1,10 @@
+# 리스트 생성
+numbers = [1, 2, 3, 4, 5]
+
+# 리스트 순회
+for number in numbers:
+    print(number)
+
+# 리스트 필터링
+even_numbers = [num for num in numbers if num % 2 == 0]
+print(even_numbers)
